abf6675ec4558e3f8cce2db82e837e252cfd87f0
Skateallday <marcinglis91@hotmail.com>
2026-01-22 15:54:09 +1000

    Added images, descriptions and times to recipes.

--- app/src/recipes/getrecipes.js
+++ app/src/recipes/getrecipes.js
@@ -15,16 +15,27 @@ function GetRecipes() {
       .catch((err) => console.error("Failed to fetch setRecipes:", err));
   }, []);
 
+  
 
   return (<>
   {recipes.map((recipes) =>(
-    <div key={recipes.recipe_id} className="hover:bg-yellow-200 border p-4 mb-4 rounded shadow">
-      <h2 className="text-xl font-bold mb-2">{recipes.recipe_name}</h2>
+    <div key={recipes.recipe_id} className="border p-4 mb-4 rounded shadow">
+      <div id="accordion" className="accordion">
+        <h2 className="accordionItem text-xl font-bold mb-2">{recipes.recipe_name}</h2>
+      <div className="accordionContent mb-4 hidden" >
       <h3>Ingredients:</h3>
       <GetIngredients recipe_id={recipes.recipe_id} />
       <h3>Steps:</h3>
       <GetInstructions recipe_id={recipes.recipe_id} />
       <button className="bg-teal-600 flex text-black font-bold px-4 py-2 rounded">Add to list</button>
+    </div>
+    <img src={recipes.image_url} alt={recipes.image_alt} className="w-[33%] h-auto mt-4 rounded" />
+    <p>{recipes.short_description}</p>
+    <p>Prep time: {recipes.prep_time_minutes} minutes</p>
+        <p>Cook time: {recipes.cook_time_minutes} minutes</p>
+    <p>Total time: {recipes.total_time_minutes} minutes</p>
+
+    </div>
     </div>
   ))}
   </>
