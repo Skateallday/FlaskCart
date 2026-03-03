d21060fef2bcf2f6f333e249c1c8ccdea943e138
Marc <marcinglis91@hotmail.com>
2026-03-03 11:32:20 +1000

    Add server files to production

---
+++ server/config.py
@@ -0,0 +1,6 @@
+import os
+from datetime import timedelta
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=2)
