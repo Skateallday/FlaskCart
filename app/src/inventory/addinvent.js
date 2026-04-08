39bbd822f27c73782bff2216dcf4a51db9f4c546
Skateallday <marcinglis91@hotmail.com>
2026-04-08 13:07:46 +1000

    Hot fix: updating value === in Add/Remove code.

--- app/src/inventory/addinvent.js
+++ app/src/inventory/addinvent.js
@@ -7,7 +7,7 @@ export default function AddButton({ item, value, onUpdate }) {
     try{
     await addToStock(item.foodName, value);  // Pass the foodName string
     
-    toast.success(`${value} ${item.foodName}${value == 1 ? '' : 's'} added`);
+    toast.success(`${value} ${item.foodName}${value === 1 ? '' : 's'} added`);
     onUpdate(item.foodName);
 
   } catch(error){
