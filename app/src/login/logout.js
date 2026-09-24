fdb477c1fbe18e92b0847e9737755b4e7ea3e48d
Skateallday <marcinglis91@hotmail.com>
2026-09-24 11:05:44 +1000

    hotfix: updating logout.js

--- app/src/login/logout.js
+++ app/src/login/logout.js
@@ -1,45 +1,45 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 import { BASE_URL } from "../config/config";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import  getCookie  from "../config/cookie"
+import getCookie from "../config/cookie";
 
-export default function Logout(){
-
+export default function Logout() {
   const navigate = useNavigate();
-  const { refreshAuth } = useAuth();       
+  const { refreshAuth } = useAuth();
   const [error, setError] = useState("");
 
-  useEffect(() =>{git push FlaskCart production
+  useEffect(() => {
     async function logout() {
-        try{
-            const csrfToken = getCookie("csrf_token");
+      try {
+        const csrfToken = getCookie("csrf_token");
 
-            const response = await fetch(`${BASE_URL}/api/auth/logout`, {
-                method: 'POST',
-                credentials: 'include',
-                headers:{
-                    "X-CSRFToken": csrfToken
-                }
-            });
-            if (!response.ok){
-                const data = await response.json();
-                throw new Error(data.error || "Unable to log out.");
-            }
+        const response = await fetch(`${BASE_URL}/api/auth/logout`, {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "X-CSRFToken": csrfToken,
+          },
+        });
 
-            await refreshAuth();
-
-            navigate("/");
-        } catch (error){
-            setError(error.message);
+        if (!response.ok) {
+          const data = await response.json();
+          throw new Error(data.error || "Unable to log out.");
         }
-    }
-    logout();
-  }, [navigate, refreshAuth])
 
-  if (error){
-    return <p>{error}</p>
+        await refreshAuth();
+        navigate("/");
+      } catch (error) {
+        setError(error.message);
+      }
+    }
+
+    logout();
+  }, [navigate, refreshAuth]);
+
+  if (error) {
+    return <p>{error}</p>;
   }
 
-  return <p>Logging out...</p>
+  return <p>Logging out...</p>;
 }
